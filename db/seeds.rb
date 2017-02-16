6510de0778414599e1bab91bd809722663513957
nachomacaso <nathanmacaso@gmail.com>
2017-02-16 16:50:33 -0600

    New logo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-Cookie.create([
-  {title: "Chocolate chip", description: "Chock-full of chocolate chips. It’s crisp on the outside and soft and chewy on the inside." },
-  {title: "PB & Dark Chocolate", description: "I’ve combined two  of my favorite flavors of all time." },
-  {title: "A lotta chocolate", description: "A double chocolate cookie for the chocolate lover." },
-  {title: "The Hawaiian", description: "Toasted coconut and lemon come together in perfect harmony." },
-  {title: "Happy Birthday", description: "A sprinkle-filled cookie. It’s a party in your mouth." },
-  {title: "Nut n Honey", description: "The sweetness of honey mixed with creamy peanut butter." },
-  {title: "Orange Creamsicle", description: "An orange a day keeps the doctor away."}
+Cookie.create!([
+  {title: "Chocolate chip", description: "Chock-full of chocolate chips. It’s crisp on the outside and soft and chewy on the inside.", price: "$10/dozen" },
+  {title: "PB & Dark Chocolate", description: "I’ve combined two  of my favorite flavors of all time.", price: "$10/dozen"},
+  {title: "A lotta chocolate", description: "A double chocolate cookie for the chocolate lover.", price: "$10/dozen" },
+  {title: "The Hawaiian", description: "Toasted coconut and lemon come together in perfect harmony.", price: "$10/dozen" },
+  {title: "Happy Birthday", description: "A sprinkle-filled cookie. It’s a party in your mouth.", price: "$10/dozen" },
+  {title: "Nut n Honey", description: "The sweetness of honey mixed with creamy peanut butter.", price: "$10/dozen" },
+  {title: "Orange Creamsicle", description: "An orange a day keeps the doctor away.", price: "$10/dozen"}
 ])
