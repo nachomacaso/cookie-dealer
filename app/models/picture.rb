e682d43e2f91e6b84ce6560733feb8ce686cd915
nachomacaso <nathanmacaso@gmail.com>
2017-04-20 23:54:17 -0500

    AWS s3

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -6,11 +6,19 @@ class Picture < ApplicationRecord
                       thumb: '100x100#',
                       small: '150x150>',
                       medium: '500x500!'
-                    }
+                    },
+                    storage: :s3,
+                    s3_credentials: Proc.new { |a| a.instance.s3_credentials }
 
   validates_attachment_content_type :image,
                                     content_type: ['image/jpg',
                                                    'image/jpeg',
                                                    'image/png',
                                                    'image/gif']
+
+  def s3_credentials
+    { bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] }
+  end
 end
